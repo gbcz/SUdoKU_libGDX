fbae0847588b5d26f855ecbcb6151ecb1d6fafbf
MaJloy <romaff.2006@gmail.com>
2025-05-23 22:05:44 +1000

    Start

---
+++ android/src/main/java/com/majloy/sudoku/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.majloy.sudoku.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.majloy.sudoku.SudokuGame;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new SudokuGame(), configuration);
+    }
+}
