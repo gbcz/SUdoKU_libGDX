079cba8779cc716eb5047d0b9550ac240bce161a
MaJloy <romaff.2006@gmail.com>
2025-05-23 22:53:48 +1000

    Shit on the screen. BRUH

--- lwjgl3/src/main/java/com/majloy/sudoku/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/majloy/sudoku/lwjgl3/Lwjgl3Launcher.java
@@ -6,6 +6,7 @@ import com.majloy.sudoku.SudokuGame;
 
 /** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
+
     public static void main(String[] args) {
         if (StartupHelper.startNewJvmIfRequired()) return; // This handles macOS support and helps on Windows.
         createApplication();
@@ -16,22 +17,28 @@ public class Lwjgl3Launcher {
     }
 
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
-        Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
-        configuration.setTitle("Sudoku");
+        Lwjgl3ApplicationConfiguration configuration1 = new Lwjgl3ApplicationConfiguration();
+        Lwjgl3ApplicationConfiguration configuration2 = new Lwjgl3ApplicationConfiguration();
+        configuration1.setTitle("Sudoku");
+        configuration2.setTitle("Sudoku");
         //// Vsync limits the frames per second to what your hardware can display, and helps eliminate
         //// screen tearing. This setting doesn't always work on Linux, so the line after is a safeguard.
-        configuration.useVsync(true);
+        configuration1.useVsync(true);
+        configuration2.useVsync(true);
         //// Limits FPS to the refresh rate of the currently active monitor, plus 1 to try to match fractional
         //// refresh rates. The Vsync setting above should limit the actual FPS to match the monitor.
-        configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate + 1);
+        configuration1.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate + 1);
+        configuration2.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate + 1);
         //// If you remove the above line and set Vsync to false, you can get unlimited FPS, which can be
         //// useful for testing performance, but can also be very stressful to some hardware.
         //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
 
-        configuration.setWindowedMode(640, 480);
+        configuration1.setWindowedMode(960, 1280);
+        configuration2.setWindowedMode(720, 1280);
         //// You can change these files; they are in lwjgl3/src/main/resources/ .
         //// They can also be loaded from the root of assets/ .
-        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
-        return configuration;
+        configuration1.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+        configuration2.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+        return configuration2;
     }
 }
